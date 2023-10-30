74822b9e5a43275dfcc50587a3628015107f5acb
MessiasViana <messiasdeviana@gmail.com>
2023-10-30 16:13:40 -0300

    adicionando projetos pessoais

--- src/components/cardsProject/index.tsx
+++ src/components/cardsProject/index.tsx
@@ -2,62 +2,89 @@ import styles from './styled.module.css';
 
 import { AiOutlineRight } from 'react-icons/ai';
 
-
 export const CardsProject = () => {
+	const projects = [
+		{
+			image: require('../../images/projects/last/muuvi.png'),
+			title: 'Muuvi',
+			link: 'https://muuvi.com.br/',
+		},
+		{
+			image: require('../../images/projects/last/jandtaxi.png'),
+			title: 'JandTaxi',
+			link: 'https://jandtaxi.com.br/',
+		},
+		{
+			image: require('../../images/projects/last/JVservicos.png'),
+			title: 'Jv serviços',
+			link: 'https://www.jvservicos.com.br/',
+		},
+	];
 
-  const projects = [
+  
+	const personalProjects = [
     {
-      image: require('../../images/projects/last/muuvi.png'),
-      title: 'Muuvi',
-      link: 'https://muuvi.com.br/'
-    },
-    {
-      image: require('../../images/projects/last/jandtaxi.png'),
-      title: 'JandTaxi',
-      link: 'https://jandtaxi.com.br/'
-    },
-    {
-      image: require('../../images/projects/last/JVservicos.png'),
-      title: 'Jv serviços',
-      link: 'https://www.jvservicos.com.br/'
-    }
-  ];
+      image: require('../../images/projects/personal/pokedex.png'),
+			title: 'Pokédex Explorer',
+			link: 'https://pokedex-explorer-seven-indol.vercel.app/',
+		},
+	];
+  
+  interface Project {
+    image: string;
+    title: string;
+    link: string;
+  }
+  
+	const renderProjects = (projectList: Project[]) =>
+		projectList.map((project, index) => (
+			<a
+				className={styles.card}
+				href={project.link}
+				target="_blank"
+				rel="noreferrer"
+				key={index}
+			>
+				<div className={styles.cardImageArea}>
+					<img src={project.image} alt="pré-visualização do site" />
+				</div>
+				<div className={styles.cardContent}>
+					<div id={styles.leftSide}>
+						<h3>{project.title}</h3>
+						<span>Veja mais</span>
+					</div>
+					<div id={styles.rightSide}>
+						<AiOutlineRight id={styles.iconCard} />
+					</div>
+				</div>
+			</a>
+		));
 
-  return (
+	return (
     <div className={styles.container}>
-      {/* Projects */}
-      <div className={styles.subTitle}>
+      {projects.length > 0 && (
+      <>
+        <div className={styles.subTitle}>
           <h2>Recentes</h2>
-      </div>
+        </div>
+        <div className={styles.projectsArea}>
+          {renderProjects(projects)}
+        </div>
+      
+      </>
+      )}
+      
 
-      <div className={styles.projectsArea}>
-        {projects.map((project, index) => (
-          <a 
-            className={styles.card}
-            href={project.link}
-            target="_blank"
-            rel='noreferrer'
-            key={index}
-          >
-            <div className={styles.cardImageArea}>
-              <img
-                src={project.image}
-                alt="imageSite"
-              />
-            </div>
-            <div className={styles.cardContent}>
-              <div id={styles.leftSide}>
-                <h3>{project.title}</h3>
-                <span>Veja mais</span>
-              </div>
-
-              <div id={styles.rightSide}>
-                <AiOutlineRight id={styles.iconCard} />
-              </div>
-            </div>
-          </a>
-        ))}
-      </div>
-    </div>
-  );
-}
+			{personalProjects.length > 0 && (
+				<>
+					<div className={styles.subTitle}>
+						<h2>Pessoais</h2>
+					</div>
+					<div className={styles.projectsArea}>
+						{renderProjects(personalProjects)}
+					</div>
+				</>
+			)}
+		</div>
+	);
+};
